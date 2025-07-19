47744ab2a42708a44576ae07ab2989125eb46a10
Joshua Graham <joshuagraham473@gmail.com>
2025-07-19 19:27:45 +0000

    compressed images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
         // Open Graph (Facebook, LinkedIn)
         { property: 'og:title', content: 'Yoofi & Matekuor — Our Wedding | 30th August 2025' },
         { property: 'og:description', content: 'Join us as we celebrate a beautiful journey of love, laughter, and forever. Yoofi & Matekuor are getting married on August 30th, 2025. 11 AM. Save the date, explore our story, and be part of the celebration.' },
-        { property: 'og:image', content: 'https://graham.africa/images/preview.jpg' },
+        { property: 'og:image', content: 'https://graham.africa/images/preview-wedding.png' },
         { property: 'og:url', content: 'https://graham.africa' },
         { property: 'og:image:width', content: '1080' },
         { property: 'og:image:height', content: '1350' },  // taller ratio for mobile
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:title', content: 'Yoofi & Matekuor — Our Wedding | 30th August 2025' },
         { name: 'twitter:description', content: 'Join us as we celebrate a beautiful journey of love, laughter, and forever. Yoofi & Matekuor are getting married on August 30th, 2025. 11 AM. Save the date, explore our story, and be part of the celebration.' },
-        { name: 'twitter:image', content: 'https://graham.africa/images/preview.jpg' }
+        { name: 'twitter:image', content: 'https://graham.africa/images/preview-wedding.png' }
 
         
       ]
